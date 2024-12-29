2d15144ebee5112878ec6059c238ff43cb68dab8
xbbazt <xbbazt@outlook.com>
2024-12-29 18:58:48 +0800

    update

--- assets/_...path_.B2oeYRDE.js
+++
@@ -1,1 +0,0 @@
-import{u as a,a as s,e as t}from"./chunks/vue-router.DNTHGQhS.js";import{e as n,K as r,P as c,N as p}from"./framework.DTsYX98Z.js";const u=n({__name:"[...path]",setup(f){const o=a(),e=s();return r(()=>{e.path!=="/"&&e.path.endsWith("/")&&o.replace(e.path.slice(0,-1))}),(i,m)=>(p(),c("div"))}});typeof t=="function"&&t(u);export{u as default};
